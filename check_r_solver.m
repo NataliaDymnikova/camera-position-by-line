a09ac6ad8df038060a9e7140dbf2c5dc7527c833
NataliaDymnikova <natalia.dymnikova@gmail.com>
2017-01-27 12:13:40 +0300

    Added get R function

--- check_r_solver.m
+++ check_r_solver.m
@@ -13,9 +13,7 @@ function [R_err] = check_r_solver()
         R_err = Inf;
         return
     end
-    R_test = [a^2 + b^2 + c^2 + d^2, 2*b*c - 2*a*d, 2*b*d + 2*a*c;
-        2*b*c + 2*a*d, a^2 - b^2 + c^2 - d^2, 2*c*d - 2*a*b;
-        2*b*d - 2*a*c, 2*c*d + 2*a*b, a^2 - b^2 - c^2 + d^2];
+    R_test = get_r_(a,b,c,d);
     [ R_err, t_err ] = test( R, t1, R_test, t1);
 end
 
--- create_cameras_and_lines/get_r_equations.m
+++ create_cameras_and_lines/get_r_equations.m
@@ -6,9 +6,7 @@ function [eqs, known, unknown, kngroups, cfg, algB]  = get_r_equations()
 
     syms a b c d real;
 
-    R = [a^2 + b^2 + c^2 + d^2, 2*b*c - 2*a*d, 2*b*d + 2*a*c;
-        2*b*c + 2*a*d, a^2 - b^2 + c^2 - d^2, 2*c*d - 2*a*b;
-        2*b*d - 2*a*c, 2*c*d + 2*a*b, a^2 - b^2 - c^2 + d^2];
+    R = get_r_abcd(a,b,c,d);
     
     eqs(1) = a^2 + b^2 + c^2 + d^2 - 1;
     for j = 1:3
