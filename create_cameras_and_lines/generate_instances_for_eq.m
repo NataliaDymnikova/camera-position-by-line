524a9586a322157db04d58dffc9d78782fc8f8d7
NataliaDymnikova <natalia.dymnikova@gmail.com>
2017-02-20 11:56:47 +0300

    fixed generate instances, but still doesn't work

--- create_cameras_and_lines/generate_instances_for_eq.m
+++ create_cameras_and_lines/generate_instances_for_eq.m
@@ -10,8 +10,8 @@ for i = 1:3
     s = randi(100,3,1);
     e = randi(100,3,1);
 
-    camera2s(:,i) = s;
-    camera2e(:,i) = e;
+    camera2s(:,i) = mod(s(1:2)*inverse(s(3), prime), prime);
+    camera2e(:,i) = mod(e(1:2)*inverse(e(3), prime), prime);
     
     temp = mod(R * s + t1, prime);
     camera1s(:,i) = mod(temp(1:2)*inverse(temp(3), prime), prime);
@@ -26,12 +26,12 @@ end
 
 l1 = mod(get_lines_from_camera(camera1s, camera1e), prime);
 l2 = mod(get_lines_from_camera(camera2s, camera2e), prime);
-l3 = mod(get_lines_from_camera (camera3s, camera3e), prime);
+l3 = mod(get_lines_from_camera(camera3s, camera3e), prime);
 
-syms a2 b2 c2 d2 real;
-R2 = get_r_abcd(a2,b2,c2,d2);
+syms a b c d;
+R2 = get_r_abcd(a,b,c,d);
 
-eqi(1) = a2^2 + b2^2 + c2^2 + d2^2 - 1;
+eqi(1) = a^2 + b^2 + c^2 + d^2 - 1;
 for j = 1:3
     eqi(j + 1) = cross(R2*l3(:,j), R2'*l1(:,j))' * l2(:,j);
 end
